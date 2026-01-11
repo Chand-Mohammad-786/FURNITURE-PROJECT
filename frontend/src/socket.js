8c1046f55c4800737bdffa2097d0105055434de2
chand <chand@gmail.com>
2026-01-11 12:41:37 +0530

    Update socket connection for production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,11 +1,19 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:9696";
-
-const socket = io(SOCKET_URL, {
+const socket = io("https://furniture-project-spox.onrender.com", {
   transports: ["websocket"],
-  autoConnect: true,
+  reconnection: true,
 });
 
 export default socket;
+
+// import { io } from "socket.io-client";
+
+// const SOCKET_URL = "http://localhost:9696";
+
+// const socket = io(SOCKET_URL, {
+//   transports: ["websocket"],
+//   autoConnect: true,
+// });
+
+// export default socket;
